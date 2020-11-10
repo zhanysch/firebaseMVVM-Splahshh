cbec08d7804ae22d9c62f63078109293b75c1512
zhanysch <zhanysch@gmail.com>
2020-11-10 15:09:37 +0600

    partOne

--- app/src/main/java/com/example/backend/ui/auth/BaseAuthActivity.kt
+++ app/src/main/java/com/example/backend/ui/auth/BaseAuthActivity.kt
@@ -1,7 +1,13 @@
 package com.example.backend.ui.auth
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 abstract class BaseAuthActivity: AppCompatActivity(){
     abstract fun getResId(): Int
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(getResId())
+    }
 }
